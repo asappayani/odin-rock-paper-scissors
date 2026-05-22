54bdc17a1ee828820c04ae282e26b3e01a2eb1bb
Aryaan Sappayani <asappayani06@gmail.com>
2026-05-22 16:15:22 -0500

    feat(ui): Add working buttons corresponding to rps actions

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
+let gameOver = false;
 
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
 
+const scoreboard = document.querySelector(".scoreboard");
+const gameCommentary = document.querySelector(".game-commentary");
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3); // we'll get 0, 1, or 2 here
@@ -13,46 +21,58 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Enter either rock, paper, or scissors.");
-}
+function playRound(humanChoice, computerChoice){
+    if (gameOver) {return};
+    if (humanChoice === computerChoice){
+        gameCommentary.textContent = "Tie! No one wins!";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        gameCommentary.textContent = "You lose! Paper beats Rock.";
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        gameCommentary.textContent = "You win! Paper beats Rock.";
 
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice === computerChoice){
-            console.log("Tie! No one wins.");
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("You lose! Paper beats Rock.");
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! Paper beats Rock.");
-            humanScore++;
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! Rock beats Scissors.");
-            humanScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors.");
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("You lose! Scissors beats Paper.");
-            computerScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! Scissors beats Paper.");
-            humanScore++;
-        }
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        gameCommentary.textContent = "You win! Rock beats Scissors.";
+
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        gameCommentary.textContent = "You lose! Rock beats Scissors.";
+
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        gameCommentary.textContent = "You lose! Scissors beats Paper";
+
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        gameCommentary.textContent = "You win! Scissors beats Paper.";
     }
 
-    for (let i = 0; i < 5; i++) {
-        console.log("Round " + (i + 1) + "");
-        const humanSelection = getHumanChoice().toLowerCase();
-        const computerSelection = getComputerChoice();
+    scoreboard.textContent = humanScore + " - " + computerScore;
 
-        playRound(humanSelection, computerSelection);
+    if (humanScore == 5) {
+        gameCommentary.textContent = "You win the game!";
+        gameOver = true;
+    } else if (computerScore == 5) {
+        gameCommentary.textContent = "You lost the game!";
+        gameOver = true;
     }
 }
+
+rockButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+paperButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+});
+
 
 
 
